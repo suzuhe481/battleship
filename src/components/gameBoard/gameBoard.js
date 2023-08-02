7b6144db7214598f1e28e449cecd5488c934a8c5
Hector <baseballhector@gmail.com>
2023-08-02 15:04:27 -0400

    receiveAttack function

--- src/components/gameBoard/gameBoard.js
+++ src/components/gameBoard/gameBoard.js
@@ -1,3 +1,11 @@
+function compareSpots(spot1, spot2) {
+  if (JSON.stringify(spot1) === JSON.stringify(spot2)) {
+    return true;
+  }
+
+  return false;
+}
+
 const gameBoardFactory = () => {
   const gameBoard = {
     shipCollection: [],
@@ -6,6 +14,28 @@ const gameBoardFactory = () => {
       this.shipCollection.push(ship);
 
       ship.location = location;
+    },
+
+    // On hit - Returns hit ship.
+    // On miss - Returns null.
+    receiveAttack(attack) {
+      // For every ship
+      for (var i = 0; i < this.shipCollection.length; i++) {
+        var ship = this.shipCollection[i];
+
+        // For every ship's location
+        for (var j = 0; j < ship.location.length; j++) {
+          var spot = ship.location[j];
+
+          if (compareSpots(spot, attack)) {
+            ship.hit();
+            return ship;
+          } else {
+          }
+        }
+      }
+
+      return null;
     },
   };
 
