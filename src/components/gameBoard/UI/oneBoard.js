7c3e701d12f7eca34d0d0822a3f4e836f6716b08
Hector <baseballhector@gmail.com>
2023-08-03 18:29:43 -0400

    Passes additional arguments into Cell()

--- src/components/gameBoard/UI/oneBoard.js
+++ src/components/gameBoard/UI/oneBoard.js
@@ -14,19 +14,22 @@ const OneBoard = () => {
     for (var col = 0; col < 11; col++) {
       // If first cell, blank
       if (row === 0 && col === 0) {
-        board.appendChild(Cell("/"));
+        board.appendChild(Cell("/", row, col));
       }
       // If starting row, letter
       else if (row === 0) {
-        board.appendChild(Cell(numToLetter(col)));
+        var label = col;
+        board.appendChild(Cell(numToLetter(label, row, col)));
       }
       // If start of a row, number
       else if (col === 0) {
-        board.appendChild(Cell(row));
+        var label = row;
+        board.appendChild(Cell(label, row, col));
       }
       // Else blank
       else {
-        board.appendChild(Cell());
+        var label = "";
+        board.appendChild(Cell(label, row, col));
       }
     }
   }
