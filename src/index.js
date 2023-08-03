9a3aa2cfd32c7a07c75949a0394e1e29ddaa9171
Hector <baseballhector@gmail.com>
2023-08-03 15:52:59 -0400

    Initialize and display game boards

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import GameBoards from "./components/gameBoard/UI/gameBoards";
+
 const Game = require("./components/game/game");
+
+const gameBoardsContainer = document.createElement("div");
+gameBoardsContainer.id = "gameboards-container";
+document.body.appendChild(gameBoardsContainer);
+
+gameBoardsContainer.appendChild(GameBoards());
 
 var game = Game();
 
