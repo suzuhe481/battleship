f4f0326f22e26b3cb9f8a0511dbbcfface85891c
Hector <baseballhector@gmail.com>
2023-08-21 20:52:44 -0400

    Gives player name as parameter to OneBoard()

--- src/components/gameBoard/UI/gameBoards.js
+++ src/components/gameBoard/UI/gameBoards.js
@@ -6,10 +6,10 @@ const GameBoards = () => {
   const gameBoards = document.createElement("div");
   gameBoards.classList.add("gameboards");
 
-  const player1Board = OneBoard();
+  const player1Board = OneBoard("Player 1");
   player1Board.id = "player1-board";
 
-  const player2Board = OneBoard();
+  const player2Board = OneBoard("Computer");
   player2Board.id = "player2-board";
 
   gameBoards.appendChild(player1Board);
